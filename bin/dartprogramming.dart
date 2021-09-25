1f27b53f35a5c3bf1a03b5221990bcf6b09b46ac
ABDUL REHMAN <mr.abdulrehman.ar@gmail.com>
2021-09-25 13:27:07 +0500

    Initial commit

---
+++ bin/dartprogramming.dart
@@ -0,0 +1,37 @@
+
+import 'variablesandmethods.dart';
+
+void main(List<String> arguments) {
+  //Data Types in dart
+  //Numbers ==> real numbers , floating point numbers
+  int a = 15; ///real number
+  double b =  125.89; // floating point number
+  //String ==> store characters and set of characters
+  String username = 'Admin';
+  String line = 'I love Pakistan';
+  //Boolean ==> store true or false value
+  bool check = false;
+  //List ==> data structure build on the base of arrays
+  List<String> names = ['Ali'];
+
+  //var ==> store dynamic type data
+  var password = 'admin';
+  //dynamic ==> store unknown data specially when we connect our app to online server
+  dynamic country = 'Pakistan';
+  //Map ==> Store json data or API data or database data
+//Call to the method
+  SUM();
+  //Call to the method
+  double res = getmultiply();
+  print('Result = ${res}');
+  //Call to the method
+  Division(15, 2);
+  Division(35, 3);
+
+  //Call to the method
+  int add = Add(12, 14);
+  print('Add = ${add  + 5}');
+  print('Add = ${Add(25, 5) }');
+
+
+}
---
+++ bin/variablesandmethods.dart
@@ -0,0 +1,37 @@
+//Functional Programming
+//Student Data
+String name = '', cnic = '' , address = '',subject = '';
+int age = 0, fee = 0;
+
+//Methods in dart ==> 4 types
+
+//Method with no return and no parameter
+void SUM()
+{
+  int x = 5 + 10;
+  print('Sum = ${x}');
+}
+
+//Method with return and no parameter
+
+double getmultiply()
+{
+  return 15.6 * 4.5;
+}
+
+
+// Method with no return and with parameter
+void Division(int a , int b)
+{
+  print('Division = ${ a / b}');
+}
+
+//Method with return and parameters
+int Add(int x, int y)
+{
+  return x + y;
+}
+
+
+
+
